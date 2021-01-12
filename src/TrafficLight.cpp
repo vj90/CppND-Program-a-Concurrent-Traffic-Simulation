ed96b567c3f154a94fddf40010a0274311603900
Vasundhara Jain <vasundhara.jain@daimler.com>
2021-01-12 15:15:58 +0100

    FP 2.a

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -54,16 +54,22 @@ the thread queue in the base class.
 }
 
 // virtual function which is executed in a thread
-void TrafficLight::cycleThroughPhases()
-{
-    // FP.2a : Implement the function with an infinite loop that measures the
-time between two loop cycles
-    // and toggles the current phase of the traffic light between red and green
-and sends an update method
-    // to the message queue using move semantics. The cycle duration should be a
-random value between 4 and 6 seconds.
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms
-between two cycles.
+void TrafficLight::cycleThroughPhases() {
+  auto start_time = std::chrono::high_resolution_clock::now();
+  while (true) {
+    srand(time(NULL));
+    auto rand_time = 2 * (rand() % 1000);
+    std::cout << "Random time = " << rand_time;
+    const auto threshold = std::chrono::milliseconds(4000 + rand_time);
+    const auto current_time = std::chrono::high_resolution_clock::now();
+    if (std::chrono::duration_cast<std::chrono::milliseconds>(
+            current_time - start_time) > threshold) {
+      start_time = current_time;
+      _currentPhase = _currentPhase == TrafficLightPhase::red
+                          ? TrafficLightPhase::green
+                          : TrafficLightPhase::red;
+      _traffic_light_phase_queue.send(std::move(_currentPhase));
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
 }
-
-*/
